fadd03367c6dd456aabde1e4c2f2653a0f5a2649
BaconBABA <130968724+BaconBABA@users.noreply.github.com>
2024-03-16 16:18:44 +0700

    Update HuntPullASword.lua

--- HuntPullASword.lua
+++ HuntPullASword.lua
@@ -1,3 +1,4 @@
+
 local Library = loadstring(Game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
 
 local function autofarm()
@@ -7,15 +8,16 @@ local function autofarm()
         end
 end
 
+local function autofarm2()
+        while _G.autofarm2 == true do
+           game:GetService("ReplicatedStorage"):WaitForChild("ClickEvent"):FireServer("Click", true)
+           task.wait()
+        end
+end
+
 local function autorebirth()
         while _G.autorebirth == true do
-        local args = {
-    [1] = "Click",
-    [2] = false
-}
-
-game:GetService("ReplicatedStorage"):WaitForChild("ClickEvent"):FireServer(unpack(args))
-
+        game:GetService("ReplicatedStorage"):WaitForChild("GameClient"):WaitForChild("Events"):WaitForChild("RemoteEvent"):WaitForChild("RebirthEvent"):FireServer()
         task.wait()
         end
 end
@@ -29,7 +31,12 @@ thehunt:CreateToggle("Auto Rebirth", function(value)
         autorebirth()
 end)
 
-thehunt:CreateToggle("Auto Click", function(value)
+thehunt:CreateToggle("Auto Click(Normal World)", function(value)
         _G.autofarm = value
         autofarm()
+end)
+
+thehunt:CreateToggle("Auto Click(Event world)", function(value)
+        _G.autofarm2 = value
+        autofarm2()
 end)
