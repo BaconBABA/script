aa6833459354616adc53da943bdfcd65d3c0dc5e
BaconBABA <130968724+BaconBABA@users.noreply.github.com>
2024-03-16 16:04:29 +0700

    Create HuntPullASword.lua

---
+++ HuntPullASword.lua
@@ -0,0 +1,29 @@
+local Library = loadstring(Game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
+
+local function autofarm()
+        while _G.autofarm == true do
+           game:GetService("ReplicatedStorage"):WaitForChild("ClickEvent"):FireServer("Click", true)
+           task.wait()
+        end
+end
+
+local function autorebirth()
+        while _G.autorebirth == true do
+        game:GetService("ReplicatedStorage"):WaitForChild("GameClient"):WaitForChild("Events"):WaitForChild("RemoteEvent"):WaitForChild("RebirthEvent"):FireServer()
+        task.wait()
+        end
+end
+
+local ui = Library:NewWindow("Combat")
+
+local thehunt = ui:NewSection("egg hunt")
+
+thehunt:CreateToggle("Auto Rebirth", function(value)
+        _G.autorebirth = value
+        autorebirth()
+end)
+
+thehunt:CreateToggle("Auto Click", function(value)
+        _G.autofarm = value
+        autofarm()
+end)
