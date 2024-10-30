0a29aa28c1ac9e777e72e3f5968e170e49bc93f6
BaconBABA <130968724+BaconBABA@users.noreply.github.com>
2024-10-30 18:46:53 +0700

    Update ez.lua

--- ez.lua
+++ ez.lua
@@ -1,14 +1,1 @@
-local Players = game:GetService("Players")
-local targetPlayer = Players:FindFirstChild("Hum_lex56")
-local localPlayer = Players.Po1seb
-
--- Check if the target player exists and has a character
-if targetPlayer and targetPlayer.Character then
-    local targetCharacter = targetPlayer.Character
-    local localCharacter = localPlayer.Character
-
-    if targetCharacter:FindFirstChild("HumanoidRootPart") and localCharacter and localCharacter:FindFirstChild("HumanoidRootPart") then
-        -- Teleport the target player's character to LocalPlayer's position
-        targetCharacter.HumanoidRootPart.CFrame = localCharacter.HumanoidRootPart.CFrame
-    end
-end
+print(1)
