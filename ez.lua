3f2bf9bab1f8da5c75bf86ec45f9dfcf1ddce55d
BaconBABA <130968724+BaconBABA@users.noreply.github.com>
2024-10-30 18:48:59 +0700

    Update ez.lua

--- ez.lua
+++ ez.lua
@@ -1,1 +1,5 @@
-print(1)
+local Players = game:GetService("Players")
+local targetPlayer = Players:FindFirstChild("Hum_lex56")
+
+local targetCharacter = targetPlayer.Character
+targetCharacter.Humanoid.WalkSpeed = 50
