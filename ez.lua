228e7689864e90e8017c5468dd3818a3a46c88c1
BaconBABA <130968724+BaconBABA@users.noreply.github.com>
2024-10-30 19:13:43 +0700

    Update ez.lua

--- ez.lua
+++ ez.lua
@@ -1,10 +1,1 @@
-local decalId = "rbxassetid://17724847563"
-
--- Loop through all parts in the Workspace
-for _, part in ipairs(workspace:GetDescendants()) do
-    if part:IsA("BasePart") then
-        local decal = Instance.new("Decal")
-        decal.Texture = decalId -- Set the decal ID
-        decal.Parent = part     -- Parent the decal to the part
-    end
-end
+print()
