8e2bb2eb2e174ae13fc6b988d213a0c677343372
BaconBABA <130968724+BaconBABA@users.noreply.github.com>
2024-10-30 19:03:54 +0700

    Update ez.lua

--- ez.lua
+++ ez.lua
@@ -3,5 +3,3 @@ sound.SoundId = "rbxassetid://18861270540" -- Set the sound ID
 sound.Volume = 10                           -- Set the volume
 sound.Parent = localPlayer.Character        -- Parent the sound to LocalPlayer's character
 sound:Play()
-wait(1)
-sound:Destroy()
