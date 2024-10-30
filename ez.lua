164ca028124147245009654c021bfcf784234874
BaconBABA <130968724+BaconBABA@users.noreply.github.com>
2024-10-30 19:04:37 +0700

    Update ez.lua

--- ez.lua
+++ ez.lua
@@ -1,5 +1,5 @@
 local sound = Instance.new("Sound")
 sound.SoundId = "rbxassetid://18861270540" -- Set the sound ID
 sound.Volume = 10                           -- Set the volume
-sound.Parent = localPlayer.Character        -- Parent the sound to LocalPlayer's character
+sound.Parent = workspace       -- Parent the sound to LocalPlayer's character
 sound:Play()
