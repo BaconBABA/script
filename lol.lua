f14d68e96fb59f1f6a751c5c8301e8d3fd3ac6df
BaconBABA <130968724+BaconBABA@users.noreply.github.com>
2024-11-03 15:26:33 +0700

    Update lol.lua

--- lol.lua
+++ lol.lua
@@ -1,6 +1,4 @@
 local player = game.Players.Hum_lex56
 local humanoidRootPart = player.Character:WaitForChild("HumanoidRootPart")
 
-while task.wait() do
 humanoidRootPart.CFrame = CFrame.new(humanoidRootPart.Position) * CFrame.Angles(0, math.rad(90), math.rad(90))
-end
