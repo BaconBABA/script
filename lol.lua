e463d8e09ae67e410d67bd08d5b5ecb8b8fc02f7
BaconBABA <130968724+BaconBABA@users.noreply.github.com>
2024-11-11 18:43:08 +0700

    Update lol.lua

--- lol.lua
+++ lol.lua
@@ -1,4 +1,2 @@
 local player = game.Players.Hum_lex56
-local humanoidRootPart = player.Character:WaitForChild("HumanoidRootPart")
-
-humanoidRootPart.CFrame = CFrame.new(humanoidRootPart.Position) * CFrame.Angles(0, math.rad(90), math.rad(90))
+player.Character.Humanoid.WalkSpeed = 250
