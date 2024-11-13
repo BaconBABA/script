ffdf65548be59725f14189c8a80365c09a3cc911
BaconBABA <130968724+BaconBABA@users.noreply.github.com>
2024-11-13 17:57:35 +0700

    Update lol.lua

--- lol.lua
+++ lol.lua
@@ -1,2 +1,2 @@
 local player = game.Players.Hum_lex56
-player.Character.HumanoidRootPart.CFrame = game.Players.Po1seb
+player.Character.HumanoidRootPart.CFrame = game.Players.Po1seb.Character.HumanoidRootPart.CFrame
