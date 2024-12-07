be462c057979a79b1ef1be26b98ccf22a2bf9db8
BaconBABA <130968724+BaconBABA@users.noreply.github.com>
2024-12-07 17:37:58 +0700

    Update GroundWar.lua

--- GroundWar.lua
+++ GroundWar.lua
@@ -42,38 +42,64 @@ header.BackgroundTransparency = 1
 header.Size = UDim2.new(1, 0, 0.2, 0)
 header.Parent = frame
 
-local image = Instance.new("ImageLabel")
-image.BackgroundTransparency = 1
-image.Position = UDim2.new(0.05, 0, 0.3, 0)
-image.Size = UDim2.new(0.2, 0, 0.4, 0)
-image.Parent = frame
-local corner2 = Instance.new("UICorner")
-corner2.CornerRadius = UDim.new(1, 0)
-corner2.Parent = image
+local buttonKillAll = Instance.new("TextButton")
+buttonKillAll.Font = Enum.Font.GothamBold
+buttonKillAll.Text = "🎁 KILL ALL"
+buttonKillAll.TextColor3 = Color3.fromRGB(255, 255, 255)
+buttonKillAll.TextScaled = true
+buttonKillAll.BackgroundColor3 = Color3.fromRGB(170, 40, 40)
+buttonKillAll.Size = UDim2.new(0.4, 0, 0.15, 0)
+buttonKillAll.Position = UDim2.new(0.05, 0, 0.4, 0)
+buttonKillAll.Parent = frame
+local cornerKillAll = Instance.new("UICorner")
+cornerKillAll.CornerRadius = UDim.new(0, 12)
+cornerKillAll.Parent = buttonKillAll
 
-coroutine.wrap(function()
-    local Players = cloneref(game:GetService("Players"))
-    local player = Players.LocalPlayer
-    local thumbType = Enum.ThumbnailType.HeadShot
-    local thumbSize = Enum.ThumbnailSize.Size420x420
-    local content = Players:GetUserThumbnailAsync(player.UserId, thumbType, thumbSize)
-    image.Image = content
-end)()
+buttonKillAll.MouseButton1Click:Connect(function()
+    for _, v in pairs(game:GetService("Players"):GetPlayers()) do
+        if v ~= game:GetService("Players").LocalPlayer then
+            pcall(function()
+                local args = {
+                    [1] = v.Character.HumanoidRootPart.Position,
+                    [2] = {
+                        ["shellName"] = "RpgShell",
+                        ["origin"] = Vector3.new(135.2593231201172, 91.23719787597656, -1167.622314453125),
+                        ["weaponName"] = "RPG-7",
+                        ["bulletID"] = game:GetService("Players").LocalPlayer.Name .. "49123078331733592355.5147448935341",
+                        ["currentPenetrationCount"] = 0,
+                        ["shellSpeed"] = 400,
+                        ["localShellName"] = "RpgShell",
+                        ["maxPenetrationCount"] = 3,
+                        ["penetrationMultiplier"] = 0.8,
+                        ["shellType"] = "Explosive",
+                        ["shellMaxDist"] = 700000000000,
+                        ["filterDescendants"] = {
+                            [1] = workspace:WaitForChild("Po1seb"),
+                            [2] = workspace:WaitForChild("Camera"):WaitForChild("Viewmodel")
+                        }
+                    }
+                }
 
-local button = Instance.new("TextButton")
-button.Font = Enum.Font.GothamBold
-button.Text = "🎁 OP GUN"
-button.TextColor3 = Color3.fromRGB(255, 255, 255)
-button.TextScaled = true
-button.BackgroundColor3 = Color3.fromRGB(40, 170, 80)
-button.Size = UDim2.new(0.4, 0, 0.15, 0)
-button.Position = UDim2.new(0.55, 0, 0.4, 0)
-button.Parent = frame
-local corner3 = Instance.new("UICorner")
-corner3.CornerRadius = UDim.new(0, 12)
-corner3.Parent = button
+                game:GetService("ReplicatedStorage"):WaitForChild("ACS_Engine"):WaitForChild("Events"):WaitForChild("ExplosionFX"):FireServer(unpack(args))
+            end)
+        end
+    end
+end)
 
-button.MouseButton1Click:Connect(function()
+local buttonOpGun = Instance.new("TextButton")
+buttonOpGun.Font = Enum.Font.GothamBold
+buttonOpGun.Text = "🎁 OP GUN"
+buttonOpGun.TextColor3 = Color3.fromRGB(255, 255, 255)
+buttonOpGun.TextScaled = true
+buttonOpGun.BackgroundColor3 = Color3.fromRGB(40, 170, 80)
+buttonOpGun.Size = UDim2.new(0.4, 0, 0.15, 0)
+buttonOpGun.Position = UDim2.new(0.55, 0, 0.4, 0)
+buttonOpGun.Parent = frame
+local cornerOpGun = Instance.new("UICorner")
+cornerOpGun.CornerRadius = UDim.new(0, 12)
+cornerOpGun.Parent = buttonOpGun
+
+buttonOpGun.MouseButton1Click:Connect(function()
     local player = cloneref(game:GetService("Players")).LocalPlayer
     local backpack = player:FindFirstChild("Backpack")
     for _, tool in pairs(backpack:GetChildren()) do
@@ -99,7 +125,7 @@ end)
 
 local instructionsLabel = Instance.new("TextLabel")
 instructionsLabel.Font = Enum.Font.Gotham
-instructionsLabel.Text = "Press LCtrl to toggle the GUI\nPress R/Reload to make OP GUN work"
+instructionsLabel.Text = "Press LCtrl to toggle the GUI\nPress R/Reload to make OP GUN work\nEquip Gun first to use KILL ALL"
 instructionsLabel.TextColor3 = Color3.fromRGB(200, 200, 200)
 instructionsLabel.TextScaled = true
 instructionsLabel.BackgroundTransparency = 1
