3387d791d896b100bde2a265f7223acbf2c81c65
BaconBABA <130968724+BaconBABA@users.noreply.github.com>
2025-01-09 18:14:27 +0700

    Update bloxgood.lua

--- bloxgood.lua
+++ bloxgood.lua
@@ -1,3 +1,20 @@
+--[[
+
+
+
+please don’t claim authorship of this script.
+
+
+
+]]--
+
+
+
+
+
+
+
+
 local Main = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
