e3ea35d0d9643202bad42926f1704b17d5b40719
BaconBABA <130968724+BaconBABA@users.noreply.github.com>
2025-01-10 20:51:47 +0700

    Update bloxgood.lua

--- bloxgood.lua
+++ bloxgood.lua
@@ -43,7 +43,7 @@ for _, fruit in ipairs(fruits:GetChildren()) do
 end
 
 local Window = Main:CreateWindow({
-    Title = "solynot " .. getgenv().script1222,
+    Title = "solynot " .. 1.2,
     SubTitle = "by SolyNot",
     TabWidth = 160,
     Size = UDim2.fromOffset(580, 460),
