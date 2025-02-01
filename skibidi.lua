4f049f1f3269e5cf20ed59f558482935d25ae368
BaconBABA <130968724+BaconBABA@users.noreply.github.com>
2025-02-01 10:02:14 +0700

    Update skibidi.lua

--- skibidi.lua
+++ skibidi.lua
@@ -1,5 +1,5 @@
 local player = game.Players:FindFirstChild("00c000u000m00")
 if player then
-    task.wait(40)
+    task.wait(10)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/BaconBABA/script/refs/heads/main/piggy.lua"))()
 end
