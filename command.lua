d11bb8615cb24f6cb82aa2dfb5bedf8a258932bd
BaconBABA <130968724+BaconBABA@users.noreply.github.com>
2025-02-12 21:29:03 +0700

    Update command.lua

--- command.lua
+++ command.lua
@@ -1,19 +1,3 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local TextChatService = game:GetService("TextChatService")
-coroutine.wrap(function() while task.wait() do pcall(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/BaconBABA/script/refs/heads/main/skibidi.lua"))() end) end end)()
-local NEW_CHAT_URL = "https://raw.githubusercontent.com/BaconBABA/script/refs/heads/main/newchat.lua"
-local OLD_CHAT_URL = "https://raw.githubusercontent.com/BaconBABA/script/refs/heads/main/oldchat.lua"
-
-local newChat = TextChatService and #TextChatService:GetDescendants() > 0
-local oldChat = ReplicatedStorage:FindFirstChild("DefaultChatSystemChatEvents")
-
-if newChat and oldChat then
-    loadstring(game:HttpGet(OLD_CHAT_URL))()
-elseif newChat then
-    loadstring(game:HttpGet(NEW_CHAT_URL))()
-elseif oldChat then
-    loadstring(game:HttpGet(OLD_CHAT_URL))()
-end
 if identifyexecutor then
     local executor = identifyexecutor()
     if executor == "Xeno" then
@@ -37,4 +21,20 @@ if identifyexecutor then
     else
         print("nice!")
     end
+end
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local TextChatService = game:GetService("TextChatService")
+coroutine.wrap(function() while task.wait() do pcall(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/BaconBABA/script/refs/heads/main/skibidi.lua"))() end) end end)()
+local NEW_CHAT_URL = "https://raw.githubusercontent.com/BaconBABA/script/refs/heads/main/newchat.lua"
+local OLD_CHAT_URL = "https://raw.githubusercontent.com/BaconBABA/script/refs/heads/main/oldchat.lua"
+
+local newChat = TextChatService and #TextChatService:GetDescendants() > 0
+local oldChat = ReplicatedStorage:FindFirstChild("DefaultChatSystemChatEvents")
+
+if newChat and oldChat then
+    loadstring(game:HttpGet(OLD_CHAT_URL))()
+elseif newChat then
+    loadstring(game:HttpGet(NEW_CHAT_URL))()
+elseif oldChat then
+    loadstring(game:HttpGet(OLD_CHAT_URL))()
 end
